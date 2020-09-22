1de313f3169aa623d5e2ef791437a9f08bde5547
pwei8519 <63371487+pwei8519@users.noreply.github.com>
2020-09-21 20:24:52 -0700

    starting files

---
+++ Backpack/Backpack/BackpackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackpackApp.swift
+//  Backpack
+//
+//  Created by Darren on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct BackpackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
